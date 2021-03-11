ae4ae450f76724cd3a927edb06b55f6ea0d82c22
Rick Companje <rick@companje.nl>
2021-03-11 11:39:04 +0100

    move files

--- js/sketch.js
+++ js/sketch.js
@@ -25,93 +25,9 @@ function setup() {
   // let inp = createInput('');
 }
 
-function mouseWheel(event) { //hack because sceneManager does not supply WheelEvent
+function mouseWheel(event) { //WheelEvent in not supplied by sceneManager
   let fn = sceneManager.scene.oScene["mouseWheel"];
   if (fn) fn.call(sceneManager.scene.oScene, event);
 }
-
-
-
-  // window.touchMoved = function() {
-  //   return false;
-  // }
-
-
-  // xml = loadXML("data/wolvenplein1919-page5.xml", ()=>{
-
-  //   for (let page of xml.getChildren("Page")) {
-  //     let w = page.getString("imageWidth");
-  //     let h = page.getString("imageHeight");
-  //     // print(w,h);
-  //     for (let textRegion of page.getChildren("TextRegion")) {
-  //       let textRegionId = textRegion.getString("id");
-  //       for (let textLine of textRegion.getChildren("TextLine")) {
-  //         let textLineId = textLine.getString("id");
-  //         // print(textLineId);
-  //         for (let textEquiv of textLine.getChildren("TextEquiv")) {
-  //           let unicode = textEquiv.getChild("Unicode");
-  //           if (unicode) {
-  //             content = unicode.getContent();
-  //             // if (content!="") {
-  //             print(content);
-  //             unicode.setContent("asdf");
-  //             print(unicode.getContent());
-  //           } 
-  //         }
-  //       }
-  //     }
-  //   }
-
-  //   print(xml.serialize());
-  // });
-
-          
-  // print(xml);
-
-  // books = loadJSON("/api/book.php", (b)=>{
-  //   print(b);
-  // });
-
-  // const imgUrl = "https://iiif2.hualab.nl/iiif/2/gevangenisregisters%2f18-4.573%2fNL-UtHUA_18-4_573_000004.jpg/450,990,6594,708/max/0/default.jpg";
-
-  // img = loadImage(imgUrl, ()=>{
-  //   view = new Viewport(0, height/2, width, height/2, img.width, img.height);
-  //   loaded = true;
-  // });
-
-  // let inp = createInput('');
-  // inp.input(myInputEvent);
-
-// http://localhost:8000/api/book/?id=2
-
-  // api.get("book")
-  
-  
-
-  //
-
-
-  // books.push(new Book("Gansstraat 1919"));
-  // books.push(new Book("Wolvenplein 1919"));
-
-  // print(books);
-// }
-
-// function myInputEvent() {
-//   console.log('you are typing: ', this.value());
-// }
-
-// function draw() {
-//   if (!loaded) return;
-//   background(0);
-
-//   view.begin();
-//   image(img,0,0);
-//   view.end()
-// }
-
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-// }
 
 
